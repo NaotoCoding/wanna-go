eb78bf70d02bfe815485c38ad0990878b3dc8ce3
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 17:12:59 +0900

    usersテーブルにnameとunique_codeを追加

--- db/schema.rb
+++ db/schema.rb
@@ -20,10 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_074426) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.string "unique_code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unique_code"], name: "index_users_on_unique_code", unique: true
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,11 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  name                   :string           not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  unique_code            :string           not null
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -15,6 +17,7 @@
 #
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_unique_code           (unique_code) UNIQUE
 #
 require 'rails_helper'
 
