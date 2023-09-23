31823561b1277099cb0e44b87cdc37fd2a200a82
ynaoto <naoto.zaniah@gmail.com>
2023-09-24 06:37:22 +0900

    参加ユーザー一覧を表示

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,2 +1,8 @@
 class GroupUsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @group = current_user.belonging_groups.find(params[:group_id])
+    @users = @group.users
+  end
 end
--- spec/requests/group_users_spec.rb
+++ spec/requests/group_users_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "GroupUsers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'ログインせずにアクションを実行した場合' do
+    it 'ログイン画面にリダイレクトされる' do
+      group = create(:group)
+      get group_group_users_path(group)
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe 'GET /groups/:group_id/group_users' do
+    it 'グループのメンバー一覧画面が表示される' do
+      user = create(:user)
+      sign_in user
+      get group_group_users_path(create(:group, owner: user))
+      expect(response).to have_http_status 200
+    end
   end
 end
