1110ecb46eece32d247aa82631359c8f3a36fafa
ynaoto <naoto.zaniah@gmail.com>
2023-09-24 04:49:54 +0900

    マイグレーションを実行

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: places
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  name        :string           not null
+#  url         :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  group_id    :bigint           not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_places_on_group_id  (group_id)
+#  index_places_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (group_id => groups.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Place < ApplicationRecord
   belongs_to :user
   belongs_to :group
