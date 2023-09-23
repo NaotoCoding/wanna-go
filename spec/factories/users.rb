3b8c51bbb18e44bc7c6d0e18f6d2e5e84ed93ef0
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 16:58:03 +0900

    userのfactoryを整備

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,6 +18,8 @@
 #
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "test#{n}@example.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
