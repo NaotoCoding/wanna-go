397e32136b87e944395db4db0f1d057a1c2d8691
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 22:56:50 +0900

    db:migrateを実行

--- app/models/accepted_invite.rb
+++ app/models/accepted_invite.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: accepted_invites
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  invite_id  :bigint           not null
+#
+# Indexes
+#
+#  index_accepted_invites_on_invite_id  (invite_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (invite_id => invites.id)
+#
 class AcceptedInvite < ApplicationRecord
   belongs_to :invite
 end
