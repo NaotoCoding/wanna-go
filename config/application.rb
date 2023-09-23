06528ba749121d174d7ef9c6ef49c23ac95f5471
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 17:06:44 +0900

    ロケールをjaに設定

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module WannaGo
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.test_framework :rspec,
         request_specs: true,
