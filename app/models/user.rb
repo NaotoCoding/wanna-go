467a7fd5b7659f8fdf67446706a9cda3ceb3493e
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 17:23:53 +0900

    unique_codeにuniquenessバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@
 #
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :unique_code, presence: true
+  validates :unique_code, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
