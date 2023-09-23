f9d97a01a1a49739c743e75f2e2d95ae753bf69c
ynaoto <naoto.zaniah@gmail.com>
2023-09-24 05:04:30 +0900

    場所作成画面を表示した

--- spec/requests/places_spec.rb
+++ spec/requests/places_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Places", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'ログインせずにアクションを実行した場合' do
+    it 'ログイン画面にリダイレクトされる' do
+      get new_group_invite_path(create(:group))
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe 'GET /groups/:group_id/places/new' do
+    it '行きたい場所作成画面が表示される' do
+      sign_in create(:user)
+      get new_group_place_path(create(:group))
+      expect(response).to have_http_status 200
+    end
   end
 end
