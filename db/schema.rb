7ebf5652a95ea13e5b3d4d55c21661b9279ccb06
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 19:45:14 +0900

    db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_101400) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_104403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "group_users", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["user_id"], name: "index_group_users_on_user_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.bigint "owner_id", null: false
@@ -37,5 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_101400) do
     t.index ["unique_code"], name: "index_users_on_unique_code", unique: true
   end
 
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
   add_foreign_key "groups", "users", column: "owner_id"
 end
