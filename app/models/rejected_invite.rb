cb991417ac6b42cc40ab7ee63ed892ffa188c402
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 23:49:13 +0900

    db:migrateを実行

--- app/models/rejected_invite.rb
+++ app/models/rejected_invite.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: rejected_invites
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  invite_id  :bigint           not null
+#
+# Indexes
+#
+#  index_rejected_invites_on_invite_id  (invite_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (invite_id => invites.id)
+#
 class RejectedInvite < ApplicationRecord
   belongs_to :invite
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_135201) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_144557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_135201) do
     t.index ["user_id"], name: "index_invites_on_user_id"
   end
 
+  create_table "rejected_invites", force: :cascade do |t|
+    t.bigint "invite_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invite_id"], name: "index_rejected_invites_on_invite_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -68,4 +75,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_135201) do
   add_foreign_key "groups", "users", column: "owner_id"
   add_foreign_key "invites", "groups"
   add_foreign_key "invites", "users"
+  add_foreign_key "rejected_invites", "invites"
 end
