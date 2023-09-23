c0986a72a2984fd60160ad8f0a040e83b6aacb19
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 22:25:13 +0900

    db:migrateを実行

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: invites
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  group_id   :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_invites_on_group_id  (group_id)
+#  index_invites_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (group_id => groups.id)
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
