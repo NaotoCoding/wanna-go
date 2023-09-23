e4b22c78d0803b78afe73a3b38b6af9b6159bed2
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 19:15:43 +0900

    nameにnull falseを追加

--- db/migrate/20230923101400_create_groups.rb
+++ db/migrate/20230923101400_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
