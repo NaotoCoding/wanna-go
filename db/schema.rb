9429b1f71163de2b4135d305b07dba745b8fa796
ynaoto <naoto.zaniah@gmail.com>
2023-09-23 19:15:56 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_074426) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_101400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_074426) do
     t.index ["unique_code"], name: "index_users_on_unique_code", unique: true
   end
 
+  add_foreign_key "groups", "users"
 end
