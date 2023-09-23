7542435019cac8e60e70e3c12011e33e0983fca8
ynaoto <naoto.zaniah@gmail.com>
2023-09-24 07:20:39 +0900

    RuboCopを適用

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
   describe 'ログインせずにアクションを実行した場合' do
     it 'ログイン画面にリダイレクトされる' do
       get me_path
@@ -8,7 +8,7 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  describe "GET /me" do
+  describe 'GET /me' do
     it 'ログインユーザーの詳細画面が表示される' do
       sign_in create(:user)
       get me_path
