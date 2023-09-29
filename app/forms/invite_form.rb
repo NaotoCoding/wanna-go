80a8b6be0725f7becfe7430a674329091bd308a2
ynaoto <naoto.zaniah@gmail.com>
2023-09-29 15:27:06 +0900

    フォームオブジェクトを作成

---
+++ app/forms/invite_form.rb
@@ -0,0 +1,32 @@
+class InviteForm
+  include ActiveModel::Model
+
+  def initialize(group, user)
+    raise ArgumentError, 'グループは必須です' if group.blank?
+
+    @group = group
+    @user = user
+  end
+
+  def save
+    return group.invites.create!(user:) if invitable_user?
+
+    false
+  end
+
+  private
+
+    attr_reader :group, :user
+
+    def invitable_user?
+      return add_error_and_return_false(:base, :not_found) if user.blank? || user.new_record?
+      return add_error_and_return_false(:base, :already_member) if group.member?(user)
+
+      true
+    end
+
+    def add_error_and_return_false(attribute, message)
+      errors.add(attribute, message)
+      false
+    end
+end
