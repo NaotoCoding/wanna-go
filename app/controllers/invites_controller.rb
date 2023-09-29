4256fdb7c1260365fad57c1c5f06f2957bbf3a2e
ynaoto <naoto.zaniah@gmail.com>
2023-09-29 15:28:52 +0900

    フォームオブジェクトを使用してエラーを表示

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -6,25 +6,22 @@ class InvitesController < ApplicationController
     @unconfirmed_invites = current_user.received_invites.unanswered
   end
 
-  def new; end
+  def new
+    @invite_form = InviteForm.new(@group, User.new)
+  end
 
   def create
-    user = User.find_by(unique_code: params[:unique_code])
-    return render_new_with_error_message('ユーザーが見つかりませんでした') if user.blank?
-    return render_new_with_error_message('既に参加しています') if @group.member?(user)
-
-    Invite.create!(group: @group, user:)
-    redirect_to group_path(@group), notice: '招待を送信しました'
+    @invite_form = InviteForm.new(@group, User.find_by(unique_code: params[:unique_code]))
+    if @invite_form.save
+      redirect_to group_path(@group), notice: '招待を送信しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
     def set_group
       @group = current_user.belonging_groups.find(params[:group_id])
-    end
-
-    def render_new_with_error_message(message)
-      flash.now[:alert] = message
-      render :new, status: :unprocessable_entity
     end
 end
