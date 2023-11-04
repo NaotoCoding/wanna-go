bb659bbdd2bd3092e28e8f8c87a3d36c4ac2a4ca
Naoto Yamaji <naoto.zaniah@gmail.com>
2023-11-04 22:23:51 +0900

    Revert "ヘッダーデザインを本番反映"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def header_link_class_names(path)
+    class_names(
+      'text-decoration-none',
+      'text-primary' => current_page?(path),
+      'text-body' => !current_page?(path)
+    )
+  end
 end
