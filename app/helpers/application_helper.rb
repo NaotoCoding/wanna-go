1ab33bf5348e6d7bfcc5ecf7f44a5d26f1c809ac
ynaoto <naoto.zaniah@gmail.com>
2023-11-04 22:19:33 +0900

    ヘッダーデザインを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def header_link_class_names(path)
-    class_names(
-      'text-decoration-none',
-      'text-primary' => current_page?(path),
-      'text-body' => !current_page?(path)
-    )
-  end
 end
